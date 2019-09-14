0b0776f95333fa66d9571dd968cf69dd6e24f34b
schuma <schuermann_marc@gmx.de>
2019-09-14 12:00:49 +0200

    Add files via upload
    Status Ausgaben angepasst
    Texte in der indego.html ergänzt geändert

--- __init__.py
+++ __init__.py
@@ -259,8 +259,19 @@ class Indego(SmartPlugin):
             # self.function(logic['name'])
             pass
 
-    
-
+    def daystring(self, zeitwert, ausgang):
+        if ausgang == 'min':
+            zeitwert = zeitwert / 60 / 24
+        if ausgang == 'std':
+            zeitwert = zeitwert / 24
+        tage, std = divmod(zeitwert, 1)
+        tage = int(tage)
+        std = std * 24
+        std, min = divmod(std,1)
+        std = int(std)
+        min = round(min * 60)
+        dayout = str(tage)+' Tage '+str(std)+' Std '+str(min)+' Min'
+        return dayout
 
     def update_item(self, item, caller=None, source=None, dest=None):
         """
@@ -1230,7 +1241,7 @@ class Indego(SmartPlugin):
                 next_time = False
                 self.logger.warning("Problem fetching {0}: {1}".format(url, e))        
             if next_time == False:
-                self.set_childitem('next_time','kein Mähen geplant')
+                self.set_childitem('next_time','nicht geplant')
                 self.logger.info("Got next-time - nothing scheduled")
             else:
                 try:
@@ -1419,6 +1430,8 @@ class Indego(SmartPlugin):
             service_counter = device_data_response['service_counter']
             self.set_childitem('service_counter',service_counter)
             self.logger.debug("service_counter " + str(service_counter))
+            service_counter = self.daystring(service_counter, 'min')
+            self.set_childitem('service_counter.dhm',service_counter)
 
             needs_service = device_data_response['needs_service']
             self.set_childitem('needs_service',needs_service)
@@ -1432,6 +1445,15 @@ class Indego(SmartPlugin):
             else:
                 self.logger.debug("ALM_MODE MANUAAAAAAAAL")
                 self.set_childitem('SMART', False)
+            if alm_mode == 'smart':
+                self.set_childitem('alm_mode.str','Übersicht SmartMow mähen:')
+            elif alm_mode == 'calendar':
+                self.set_childitem('alm_mode.str','Übersicht Kalender mähen:')
+            elif alm_mode == 'manual':
+                self.set_childitem('alm_mode.str','')
+            else:
+                self.set_childitem('alm_mode.str','unbekannt')
+          
             self.logger.debug("alm_mode " + str(alm_mode))
 
             bareToolnumber = device_data_response['bareToolnumber']
@@ -1479,21 +1501,22 @@ class Indego(SmartPlugin):
         #body = {"count":1,"interval":1}
         #state_response = self.post_url(self.indego_url + 'alms/' + self.alm_sn + '/requestPosition', self.context_id, body)
         #states = state_response
-        state__str = {0: ['Lese Status', 'unknown'], 257: ['lädt', 'dock'], 258: ['docked', 'dock'],
-                      259: ['Docked-Softwareupdate', 'dock'], 260: ['Docked', 'dock'], 261: ['docked', 'dock'],
-                      262: ['docked - lädt Karte', 'dock'], 263: ['docked-speichert Karte', 'dock'],
-                      513: ['mäht', 'moving'], 514: ['bestimme Ort', 'moving'], 515: ['lade Karte', 'moving'],
-                      516: ['lerne Garten', 'moving'], 517: ['Pause', 'pause'], 518: ['schneide Rand', 'moving'],
-                      519: ['stecke fest', 'hilfe'],523 :['Spot Mow','dock'],524 : ['zufälliges Mähen','dock'],
-                      769: ['fährt in Station', 'moving'],
-                      770: ['fährt in Station', 'moving'], 771: ['fährt zum Laden in Station', 'moving'],
-                      772: ['fährt in Station – Mähzeit beendet', 'moving'],
-                      773: ['fährt in Station - überhitzt', 'help'], 774: ['fährt in Station', 'moving'],
-                      775: ['fährt in Station - fertig gemäht', 'moving'],
-                      776: ['fährt in Station - bestimmt Ort', 'moving'], 1025: ['Diagnosemodus', 'unknown'],
-                      1026: ['Endoflive', 'hilfe'], 1281: ['Softwareupdate', 'dock'],
-                      1537: ['Stromsparmodus','dock'],
-                      64513:['wacht auf','dock']}
+        state__str = {0: ['liest den Status', 'unknown'], 257: ['lädt den Akku', 'dock'], 258: ['ist angedockt', 'dock'],
+                      259: ['Softwareupdate!', 'dock'], 260: ['ist angedockt', 'dock'], 261: ['ist angedockt', 'dock'],
+                      262: ['lädt die Karte', 'dock'], 263: ['speichert die Karte', 'dock'], 
+					  266: ['???', 'dock'],
+                      513: ['mäht', 'moving'], 514: ['bestimmt den Ort', 'moving'], 515: ['lädt die Karte', 'moving'],
+                      516: ['lernt den Garten', 'moving'], 517: ['macht Pause', 'pause'], 518: ['schneidet den Rand', 'moving'],
+                      519: ['steckt fest!', 'hilfe'],523 :['mäht im Spot Mow','moving'],524 : ['mäht zufällig','moving'],
+                      768: ['????', 'moving'], 769: ['fährt in die Station', 'moving'],
+                      770: ['fährt in die Station', 'moving'], 771: ['fährt zum Laden in die Station', 'moving'],
+                      772: ['hat die Mähzeit beendet - fährt zurück', 'moving'],
+                      773: ['ist überhitzt - fährt zurück', 'help'], 774: ['fährt in Station', 'moving'],
+                      775: ['hat fertig gemäht - fährt zurück', 'moving'],
+                      776: ['bestimmt seine Position', 'moving'], 1025: ['Diagnosemodus!', 'unknown'],
+                      1026: ['Endoflive', 'hilfe'], 1281: ['Softwareupdate!', 'dock'],
+                      1537: ['Stromsparmodus!','dock'],
+                      64513:['wacht auf...','dock']}
         
         
         state_response = self.get_url(self.indego_url + 'alms/' + self.alm_sn + '/state', self.context_id)
@@ -1565,10 +1588,14 @@ class Indego(SmartPlugin):
             total_operate = states['runtime']['total']['operate']
             self.set_childitem('runtimeTotalOperationMins',total_operate)
             self.logger.debug("total_operate " + str(total_operate))
+            total_operate = self.daystring(total_operate, 'min')
+            self.set_childitem('runtimeTotalOperationMins.dhm',total_operate)
 
             total_charge = states['runtime']['total']['charge']
             self.set_childitem('runtimeTotalChargeMins',total_charge)
             self.logger.debug("total_charge " + str(total_charge))
+            total_charge = self.daystring(total_charge, 'min')
+            self.set_childitem('runtimeTotalChargeMins.dhm',total_charge)
 
             session_operate = states['runtime']['session']['operate']
             self.set_childitem('runtimeSessionOperationMins',session_operate)
@@ -1718,5 +1745,4 @@ class WebInterface(SmartPluginWebIf):
         return tmpl.render(p=self.plugin,
                            items=sorted(plgitems, key=lambda k: str.lower(k['_path'])),
                            item_count=item_count)
-
 
